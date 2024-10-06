415e4f51afea8830fd98a4f03f2bf2646ce2326b
Andy Krohg <akrohg@redhat.com>
2024-10-06 15:52:19 -0400

    Use bot name

--- k8s.js
+++ k8s.js
@@ -1,41 +1,20 @@
 const k8s = require('@kubernetes/client-node');
 const fs = require('fs');
-const token = fs.readFileSync("/var/run/secrets/kubernetes.io/serviceaccount/token").toString();
 const namespace = fs.readFileSync("/var/run/secrets/kubernetes.io/serviceaccount/namespace").toString();
 
-const cluster = {
-    name: 'internal-server',
-    server: 'https://kubernetes.default.svc',
-    skipTLSVerify: true
-};
-
-const user = {
-    token: token
-};
-
-const context = {
-    name: 'my-context',
-    user: user.name,
-    cluster: cluster.name,
-};
-
 const kc = new k8s.KubeConfig();
-kc.loadFromOptions({
-    clusters: [cluster],
-    users: [user],
-    contexts: [context],
-    currentContext: context.name,
-});
+kc.loadFromCluster();
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 
 async function createBot(username) {
     let pod = k8s.loadYaml(fs.readFileSync(process.cwd() + "/mindcraft_pod_template.yaml").toString());
 
     // TODO: Update pod with my own env variables for Model server and stuff here
-
-    const res = await k8sApi.createNamespacedPod(namespace, pod);
     let matches = res.body.metadata.name.matchAll(/-(.[a-z0-9]+)$/g);
     let botName = `${username}_${matches.next().value[1]}`;
+    pod.spec.containers[0].env.push({name: "BOT_NAME", value: botName});
+
+    const res = await k8sApi.createNamespacedPod(namespace, pod);
     console.log(`Bot ${botName} created`);
     return botName;
 };
