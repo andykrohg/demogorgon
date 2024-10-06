521a760f5986658fca2d7cbf8ec9ca40b14d8cf1
Andy Krohg <akrohg@redhat.com>
2024-10-06 15:54:27 -0400

    must pass the prefix instead

--- k8s.js
+++ k8s.js
@@ -10,11 +10,11 @@ async function createBot(username) {
     let pod = k8s.loadYaml(fs.readFileSync(process.cwd() + "/mindcraft_pod_template.yaml").toString());
 
     // TODO: Update pod with my own env variables for Model server and stuff here
+    pod.spec.containers[0].env.push({name: "USERNAME", value: botName});
+    const res = await k8sApi.createNamespacedPod(namespace, pod);
     let matches = res.body.metadata.name.matchAll(/-(.[a-z0-9]+)$/g);
     let botName = `${username}_${matches.next().value[1]}`;
-    pod.spec.containers[0].env.push({name: "BOT_NAME", value: botName});
 
-    const res = await k8sApi.createNamespacedPod(namespace, pod);
     console.log(`Bot ${botName} created`);
     return botName;
 };
