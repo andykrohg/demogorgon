b1eed49f412062de323fd4d5887c9c0e64239344
Andy Krohg <akrohg@redhat.com>
2024-10-06 15:58:43 -0400

    username, not botname

--- k8s.js
+++ k8s.js
@@ -10,7 +10,7 @@ async function createBot(username) {
     let pod = k8s.loadYaml(fs.readFileSync(process.cwd() + "/mindcraft_pod_template.yaml").toString());
 
     // TODO: Update pod with my own env variables for Model server and stuff here
-    pod.spec.containers[0].env.push({name: "USERNAME", value: botName});
+    pod.spec.containers[0].env.push({name: "USERNAME", value: username});
     const res = await k8sApi.createNamespacedPod(namespace, pod);
     let matches = res.body.metadata.name.matchAll(/-(.[a-z0-9]+)$/g);
     let botName = `${username}_${matches.next().value[1]}`;
